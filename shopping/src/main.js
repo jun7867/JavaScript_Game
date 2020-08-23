331f23ac070e940b19fcf6d7b413d09b20894b98
남쭌 <jjune095@naver.com>
2020-08-24 01:11:50 +0900

    displayItems shooping game

--- shopping/src/main.js
+++ shopping/src/main.js
@@ -6,6 +6,23 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+//받아온 items를 display 화면에 보여주기
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+function createHTMLString(item) {
+  return `
+  <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>`;
+}
+
+//main
+//아이템 동적으로 받아오기
 loadItems().then((items) => {
   console.log(items);
+  displayItems(items); // 화면에 출력.
 });
