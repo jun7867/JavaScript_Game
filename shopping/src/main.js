6bec6d7cf1f934f4c0676bdd3d27c019a8c2941f
남쭌 <jjune095@naver.com>
2020-08-24 01:29:37 +0900

    button event filter shopping

--- shopping/src/main.js
+++ shopping/src/main.js
@@ -9,9 +9,10 @@ function loadItems() {
 //받아온 items를 display 화면에 보여주기
 function displayItems(items) {
   const container = document.querySelector(".items");
-  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join(""); // join을 해서 붙여줘야함
 }
 
+//items object를 HTML li형태로 리턴
 function createHTMLString(item) {
   return `
   <li class="item">
@@ -20,9 +21,33 @@ function createHTMLString(item) {
   </li>`;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  const filtered = items.filter((item) => item[key] === value);
+  displayItems(filtered);
+}
+
+//버튼을 클릭했을때 filter
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".btn__container");
+  logo.addEventListener("click", () => {
+    displayItems(items);
+  });
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
 //main
 //아이템 동적으로 받아오기
 loadItems().then((items) => {
   console.log(items);
   displayItems(items); // 화면에 출력.
+  setEventListeners(items);
 });
