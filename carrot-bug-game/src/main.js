1ad132faabe43e085e0ce26c7f269698c6e92bb7
남쭌 <jjune095@naver.com>
2020-09-02 02:24:36 +0900

    carrot game gameStart

--- carrot-bug-game/src/main.js
+++ carrot-bug-game/src/main.js
@@ -1,7 +1,18 @@
 "use strict";
 const CARROT_SIZE = 80;
+const CARRPT_COUNT = 5;
+const BUG_COUNT = 5;
 const field = document.querySelector(".game__field");
 const field_Rect = field.getBoundingClientRect();
+const startBtn = document.querySelector(".game__button");
+const gameScore = document.querySelector(".game__score");
+const gameTimer = document.querySelector(".game__timer");
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+function stayCarrot() {}
 
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
@@ -25,10 +36,38 @@ function addItem(className, count, imgPath) {
     field.appendChild(item);
   }
 }
+function startGame() {
+  field.innerHTML = "";
+  gameScore.innerText = CARRPT_COUNT;
+  addItem("carrot", CARRPT_COUNT, "img/carrot.png");
+  addItem("bug", BUG_COUNT, "img/bug.png");
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+function showStopButton() {
+  const icon = startBtn.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.remove("fa-play");
+}
+function showTimerAndScore() {
+  gameScore.style.visibility = "visible";
+  gameTimer.style.visibility = "visible";
+}
+function startGameTimer() {}
+function stopGame() {}
 
 function init() {
-  addItem("carrot", 5, "img/carrot.png");
-  addItem("bug", 5, "img/bug.png");
+  startBtn.addEventListener("click", () => {
+    if (started) {
+      stopGame();
+    } else {
+      startGame();
+    }
+    started = !started;
+
+    stayCarrot();
+  });
 }
 
 init();
